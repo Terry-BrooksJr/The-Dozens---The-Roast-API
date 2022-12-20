6c369c66717be9669f45b130c12bfcc60d9006b7
Terry Brooks, Jr <Terry.Arthur@BrooksJr.com>
2022-12-19 21:37:16 -0600

    Updated Models Per Codacy Recommendations

--- database/models.py
+++ database/models.py
@@ -1,5 +1,4 @@
-from flask import jsonify
-from flask_bcrypt import check_password_hash, generate_password_hash
+
 from flask_jwt_extended import create_access_token, get_jwt
 import pendulum
 from utils.gatekeeper import GateKeeper
